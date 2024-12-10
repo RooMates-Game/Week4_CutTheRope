7a3ad239b9b5728be24a3e7adfca88d7197d7fd5
ron_shuster <ronshuster99@gmail.com>
2024-12-11 00:54:36 +0200

    Update Rope.cs

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -2,31 +2,30 @@ using UnityEngine;
 
 public class Rope : MonoBehaviour
 {
-		public Rigidbody2D hook;
-	public GameObject linkPrefab;
-	public Weight weigth;
-	[SerializeField] private int links = 7;
-	
-	
-	GameObject link;
+    public Rigidbody2D hook;
+    public GameObject linkPrefab;
+    public Weight weigth;
+    [SerializeField] private int links = 7;
 
-	void Start ()
-	{
-		GenerateRope();
-	}
+    private GameObject link;
 
-	void GenerateRope ()
-	{
-		Rigidbody2D previousRB = hook;
-		
-		for (int i = 0; i < links; i++) //create ropes
-		{
-			link = Instantiate(linkPrefab, transform);
-			HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
-			joint.connectedBody = previousRB;
-			previousRB = link.GetComponent<Rigidbody2D>();
-			
-		}
-		weigth.ConnectRopeEnd(link.GetComponent<Rigidbody2D>());
-	}
+    void Start()
+    {
+        GenerateRope();
+    }
+
+    void GenerateRope()
+    {
+        Rigidbody2D previousRB = hook;
+
+        for (int i = 0; i < links; i++) // Create ropes
+        {
+            link = Instantiate(linkPrefab, transform);
+            HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
+            joint.connectedBody = previousRB;
+            previousRB = link.GetComponent<Rigidbody2D>();
+        }
+
+        weigth.ConnectRopeEnd(link.GetComponent<Rigidbody2D>());
+    }
 }
