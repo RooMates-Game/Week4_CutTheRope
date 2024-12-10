fbfe80d5117bd7ede078758f2a0745968c9a3dd0
ron_shuster <ronshuster99@gmail.com>
2024-12-11 00:56:58 +0200

    Update RopeCutter.cs

--- Assets/Scripts/RopeCutter.cs
+++ Assets/Scripts/RopeCutter.cs
@@ -1,54 +1,50 @@
 using UnityEngine;
 
 public class RopeCutter : MonoBehaviour
-{	
-	Rigidbody2D rb;
-	Camera Cam;
-	[SerializeField] private string triggeringTag;
-	[SerializeField] private float bladeTimer = 0.05f;
+{
+    private Rigidbody2D rb;
+    private Camera Cam;
+    [SerializeField] private string triggeringTag;
+    [SerializeField] private float bladeTimer = 0.05f;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Cam = Camera.main;
+    }
 
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rb.position = Cam.ScreenToWorldPoint(Input.mousePosition);
+            Invoke(nameof(Visible_blade), bladeTimer);
 
-	void Start()
-	{
-       rb = GetComponent<Rigidbody2D>();
-	   Cam = Camera.main;
-	}
+            RaycastHit2D hit = Physics2D.Raycast(Cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag(triggeringTag))
+                {
+                    Destroy(hit.collider.gameObject); // cut blade
 
-	void Update () 
-	{
-		if (Input.GetMouseButton(0))
-		{
-			rb.position = Cam.ScreenToWorldPoint(Input.mousePosition);
-			Invoke(nameof(Visible_blade), bladeTimer);
-			
-			RaycastHit2D hit = Physics2D.Raycast(Cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-			if (hit.collider != null)
-			{
-				if (hit.collider.CompareTag(triggeringTag))
-				{
-					Destroy(hit.collider.gameObject);//cut blade
-				    
-					// remove all ropes
-					int count_child = hit.transform.parent.gameObject.transform.childCount + 1; // +1 for the anchor
-					for (int i = 0 ; i < count_child-1; i++)
-					{
-						Destroy(hit.transform.parent.gameObject.transform.GetChild(i).gameObject);
-					}
-				}
-			}
-		}
+                    // remove all ropes
+                    int count_child = hit.transform.parent.gameObject.transform.childCount + 1; // +1 for the anchor
+                    for (int i = 0; i < count_child - 1; i++)
+                    {
+                        Destroy(hit.transform.parent.gameObject.transform.GetChild(i).gameObject);
+                    }
+                }
+            }
+        }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
 
-		if(Input.GetMouseButtonDown(0))
-		{
-           gameObject.transform.GetChild(0).gameObject.SetActive(false);
-		}
-	}
-	void Visible_blade()
-	{
-       gameObject.transform.GetChild(0).gameObject.SetActive(true);
-	}
-
-	
+    void Visible_blade()
+    {
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+    }
 }
